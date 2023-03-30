d90f80bb362719ea73babfe4449174b9f58cb66b
byungjoo <byungjoo104@gmail.com>
2023-03-30 21:26:38 +0900

    add eslint & grid system

--- src/App.vue
+++ src/App.vue
@@ -1,82 +1,66 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-app>
+        <v-app-bar
+            app
+            color="primary"
+            dark
+        >
+            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
 
-      <v-spacer></v-spacer>
+            <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-      <v-navigation-drawer app>
-          <v-list>
-              <v-list-item>
-                  <v-list-item-content>
-                      <v-list-item-title class="title">
-                          Application
-                      </v-list-item-title>
-                      <v-list-itme-subtitle>
-                          subtext
-                      </v-list-itme-subtitle>
-                  </v-list-item-content>
-              </v-list-item>
-          </v-list>
+            <v-btn
+                href="https://github.com/vuetifyjs/vuetify/releases/latest"
+                target="_blank"
+                text
+            >
+                <span class="mr-2">Latest Release</span>
+                <v-icon>mdi-open-in-new</v-icon>
+            </v-btn>
+        </v-app-bar>
+        <v-navigation-drawer v-model="drawer" app>
+            <v-list>
+                <v-list-item>
+                    <v-list-item-content>
+                        <v-list-item-title class="title">
+                            Application
+                        </v-list-item-title>
+                        <v-list-itme-subtitle>
+                            subtext
+                        </v-list-itme-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
 
-          <v-divider></v-divider>
+            <v-divider></v-divider>
 
-          <v-list-item v-for="item in items" :key="item.title" link :to="item.to">
-              <v-list-item-icon>
-                  <v-icon>{{item.icon}}</v-icon>
-              </v-list-item-icon>
-              <v-list-item-content>
-                  <v-list-item-title>{{ item.title }}</v-list-item-title>
-              </v-list-item-content>
-          </v-list-item>
-      </v-navigation-drawer>
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
+            <v-list-item v-for="item in items" :key="item.title" link :to="item.to">
+                <v-list-item-icon>
+                    <v-icon>{{ item.icon }}</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                    <v-list-item-title>{{ item.title }}</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+        </v-navigation-drawer>
+        <v-main>
+            <router-view/>
+        </v-main>
+    </v-app>
 </template>
 
 <script>
 
 export default {
-  name: 'App',
+    name: 'App',
 
-  data: () => ({
-    items: [
-        { title: "Dashboard", icon: "mdi-view-dashboard", to: "/"},
-        { title: "GridSystem", icon: "mdi-view-dashboard", to: "/grid-system"},
-    ]
-  }),
+    data: () => ({
+        drawer: true,
+        items: [
+            {title: "Dashboard", icon: "mdi-view-dashboard", to: "/"},
+            {title: "GridSystem", icon: "mdi-view-dashboard", to: "/grid-system"},
+        ]
+    }),
 };
 </script>
